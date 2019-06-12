4ec8eb659503ab22efce80f6bfc32845a90c0948
DemonV4 <jonatadss@hotmail.com>
2019-06-12 01:14:24 -0300

    Comitando Curriculo

---
+++ header.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-success">
+    <div class="container d-flex justify-content-around">
+        <img class="img-fluid" src="#">
+        <!-- <a class="navbar-brand" href="#"></a> -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-success"
+            aria-controls="navbar-success" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbar-success">
+            <div class="navbar-collapse-header">
+                <div class="row">
+                    <div class="col-6 collapse-brand">
+                        <a href="index.html">
+                            <img src="assets/img/brand/blue.png">
+                        </a>
+                    </div>
+                    <div class="col-6 collapse-close">
+                        <button type="button" class="navbar-toggler" data-toggle="collapse"
+                            data-target="#navbar-success" aria-controls="navbar-success" aria-expanded="false"
+                            aria-label="Toggle
+                                    navigation">
+                            <span></span>
+                            <span></span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <ul class="navbar-nav ml-lg-auto">
+                <li class="nav-item mr-1">
+                <!-- link para a pagina home -->
+                    <a class="nav-link nav-link-icon" href="index.php">
+                        <i class="fa fa-home"></i>
+                        <span class="nav-link-inner--text">Home</span>
+                    </a>
+                </li>
+            <ul>
+        </div>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,313 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Curriculo</title>
+    <link rel="stylesheet" href="./argon-design-system-master/assets/vendor/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="./argon-design-system-master/assets/css/argon.min.css">
+    <link href="./argon-design-system-master/assets/vendor/nucleo/css/nucleo.css" rel="stylesheet">
+    <link rel="stylesheet" href="./_css/estilo.css">
+</head>
+
+<body style="background-color:#babaca">
+    <?php include "header.php"?>
+
+    <section class="container d-flex justify-content-center mt-5 mb-5" style="background-color:#FFFFFF" ;>
+        <div class="row mt-5 mg-5">
+            <!-- Nome -->
+            <div class="col-md-12">
+                <h1 class="display 3 d-flex justify-content-center" style="color:">Jonatan da Silva Souza</h1>
+                <h3 class="display 1 d-flex justify-content-center" style="color:">Analista de Sistemas</h3>
+            </div>
+            <!-- Endereço -->
+            <div class="col-md-3 d-flex justify-content-center">
+                <i class="fa fa-map-marker" style="color:#D98719;"></i>
+                <label>Casa do caralho</label>
+            </div>
+
+            <!-- Telefone -->
+            <div class="col-md-3 d-flex justify-content-center">
+                <i class="fa fa-phone" style="color:#D98719;"></i>
+                <label>+55 11 944818010</label>
+            </div>
+
+            <!-- email -->
+            <div class="col-md-3 d-flex justify-content-center">
+                <i class="fa fa-envelope" style="color:#D98719;"></i>
+                <label>jonatadss@hotmail.com</label>
+            </div>
+
+            <!-- Site de exemplo -->
+            <div class="col-md-3 d-flex justify-content-center">
+                <i class="fa fa-envelope" style="color:#D98719;"></i>
+                <label>Exemplo.com</label>
+            </div>
+            
+            <div class="col-md-12">
+                <hr class="mt-1">
+            </div>
+            <!-- Resumo -->
+            <div class="col-md-3 I">
+                <h5 class="H5">RESUMO</h5>
+                <p class="lead" style="font-size:15px;">Coloque Aqui informações relativas a este tópico
+                    que sejam suficientemente
+                    esclarecedoras sobre a situação atual do mercado. Coloque aqui informações relativas a este tópico
+                </p>
+            </div>
+
+            <div class="col-md-2" >
+                <h5 class="H5">EXPERIÊNCIA</h5>
+                <p class="lead mb-0" style="font-size:15px;">2017 - 2018</p>
+                <p class="lead mt-0" style="font-size:15px;">Nome da Empresa</p>
+            </div>
+
+            <div class="col-md-6">
+                <h5 class="mt-5">WEB DESIGN</h5>
+                <p class="lead " style="font-size:15px;">Coloque Aqui Informações relativas a este tópico que
+                    sejam suficientement esclarecedoras sobre a situação atual no mercado Coloque aqui informações
+                    relativas a este tópico</p>
+            </div>
+
+
+            <!-- Resumo 2 -->
+            <div class="col-md-3 I">
+                <p class="lead" style="font-size:15px;">Coloque Aqui informações relativas a este tópico
+                    que sejam suficientemente
+                    esclarecedoras sobre a situação atual do mercado. Coloque aqui informações relativas a este tópico
+                </p>
+            </div>
+
+            <div class="col-md-2">
+                <p class="lead mb-0" style="font-size:15px;">2017 - 2018</p>
+                <p class="lead mt-0" style="font-size:15px;">Nome da Empresa</p>
+            </div>
+
+            <div class="col-md-6">
+            <h5 class="mt-3">WEB DESIGN</h5>
+                <p class="lead" style="font-size:15px;">Coloque Aqui Informações relativas a este tópico que
+                    sejam suficientement esclarecedoras sobre a situação atual no mercado Coloque aqui informações
+                    relativas a este tópico</p>
+            </div>
+
+
+            <!-- Resumo 3-->
+            <div class="col-md-3 I">
+                <p class="lead" style="font-size:15px;">Coloque Aqui informações relativas a este tópico
+                    que sejam suficientemente
+                    esclarecedoras sobre a situação atual do mercado. Coloque aqui informações relativas a este tópico
+                </p>
+            </div>
+
+            <div class="col-md-2">
+                <p class="lead mb-0" style="font-size:15px;">2017 - 2018</p>
+                <p class="lead mt-0" style="font-size:15px;">Nome da Empresa</p>
+            </div>
+
+            <div class="col-md-6">
+            <h5 class="mt-3">WEB DESIGN</h5>
+                <p clas="lead " style="font-size:15px;">Coloque Aqui Informações relativas a este tópico que
+                    sejam suficientement esclarecedoras sobre a situação atual no mercado Coloque aqui informações
+                    relativas a este tópico</p>
+            </div>
+
+            <!-- competencias -->
+            <!-- microsoft -->
+            <div class="col-md-3 I">
+                <h5 class="display 3 mb-0 H5">COMPETÊNCIAS</h5>
+                <div class="progress-wrapper row">
+                    <div class="progress-info col-md-6" >
+                        <label>Microsoft Office</label>
+                    </div>
+                    <div class="progress col-md-6" style="padding:0;">
+                        <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 90%;"></div>
+                    </div>
+                </div>
+                <!-- Photoshop CC -->
+                <div class="progress-wrapper row mt-0 mb-0">
+                    <div class="progress-info col-md-6" >
+                        <label>Photoshop CC</label>
+                    </div>
+                    <div class="progress col-md-6" style="padding:0;">
+                        <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 80%;"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-2">
+                <h5 class="H5">FORMAÇÃO</h5>
+                <span class="lead mt-0 mb-0" style="font-size:15px;">2017 - 2018</span>
+                <span class="lead mt-0" style="font-size:15px;">Universidade nova de lisboa</span>
+            </div>
+
+            <div class="col-md-6">
+                <h6>Mestrado em Engenharia Informática</h6>
+                <p clas="lead " style="font-size:15px;">Coloque Aqui Informações relativas a este tópico que 
+                sejam suficientement esclarecedoras sobre a situação atual no mercado.</p>
+            </div>
+
+            <!-- Premiere CC -->
+            <div class="col-md-3 I">
+                <div class="progress-wrapper row">
+                    <div class="progress-info col-md-6" >
+                        <label>Premiere CC</label>
+                    </div>
+                    <div class="progress col-md-6" style="padding:0;">
+                        <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 40%;"></div>
+                    </div>
+                </div>
+
+                 <!-- ilustrator CC -->
+                 <div class="progress-wrapper row">
+                    <div class="progress-info col-md-6">
+                        <label>Ilustrator CC</label>
+                    </div>
+                    <div class="progress col-md-6" style="padding:0;">
+                        <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 80%;"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-2">
+                <p class="lead m-0" style="font-size:15px;">2017 - 2018</p>
+                <p class="lead m-0" style="font-size:15px;">Universidade nova de Lisboa</p>
+            </div>
+
+            <div class="col-md-6">
+                <h6>Licenciatura Em Engenharia Informática</h6>
+                <p clas="lead" style="font-size:15px;">Coloque Aqui Informações relativas a este tópico que
+                    sejam suficientement esclarecedoras sobre a situação atual no mercado.</p>
+            </div>
+
+            <div class="col-md-3 I">
+                <h5 class="H5">ESPECIALIDADE</h5>
+                <p class="lead m-0" style="font-size:15px;">+ Teoria da Cor</p>
+                <p class="lead m-0" style="font-size:15px;">- Boostrap</p>
+                <p class="lead m-0" style="font-size:15px;">- WordPress</p>
+                <p class="lead m-0" style="font-size:15px;">+ User experience</p>
+                <p class="lead m-0" style="font-size:15px;">+ WireFrames</p>
+            </div>
+
+            <!-- Linguas -->
+            <div class="col-md-9">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h5 class="H5">LÍNGUAS</h5>
+                    </div>
+
+                    <!-- Português -->
+                    <div class="col-md-4">
+                        <h6>Português</h6>
+                        <a class="lbln">Fala: </a>
+                        <label>Nativo</label><br/>
+
+                        <a class="lbln">Escrita: </a>
+                        <label>Nativo</label><br/>
+
+                        <a class="lbln">Compreenção: </a>
+                        <label>Nativo</label>
+                    </div>
+
+                    <!-- Inglês -->
+                    <div class="col-md-4">
+                        <h6>Inglês</h6>
+                        <a class="lbln">Fala: </a>
+                        <label>Bom</label><br/>
+
+                        <a class="lbln">Escrita: </a>
+                        <label>Razoável</label><br/>
+
+                        <a class="lbln">Compreenção: </a>
+                        <label>Bom</label>
+                    </div>
+
+                    <!-- Espanhol -->
+                    <div class="col-md-4">
+                        <h6>Espanhol</h6>
+                        <a class="lbln">Fala: </a>
+                        <label>Iniciante</label><br/>
+    
+                        <a class="lbln">Escrita: </a>
+                        <label>Iniciante</label><br/>
+    
+                        <a class="lbln">Compreenção: </a>
+                        <label>Razoável</label>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Redes Socias -->
+            <div class="col-md-3 I">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h5 class="H5">Redes Sociais</h5>
+                    </div>
+                    <div class="col-md-6">
+                        <!-- facebook -->
+                        <i class="fa fa-facebook" style="color:#D98719;"></i>
+                        <label style="font-size:8px">/jonatan.souza.3781</label>
+                        
+                        <!-- Instagran -->
+                        <i class="fa fa-skype" style="color:#D98719;"></i>
+                        <label style="font-size:8px">/jonatan.souza.3781</label>
+                    </div>
+                    <div class="col-md-6">
+                        <!-- Skype -->
+                        <i class="fa fa-instagram" style="color:#D98719;"></i>
+                        <label style="font-size:8px">/jonatan.souza.3781</label>
+                        
+                        <!-- Linkedin -->
+                        <i class="fa fa-linkedin" style="color:#D98719;"></i>
+                        <label style="font-size:8px">/jonatan.souza.3781</label>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Referencias -->
+            <div class="col-md-9 mb-4">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h5 class="H5">REFERÊNCIAS</h5>
+                    </div>
+
+                    <!-- D.João Carvalho -->
+                    <div class="col-md-4">
+                        <h6 class="mb-0">D.João Carvalho</h6>
+                        <a class="lbln mt-0">Diretor de Departamento de informatica da XPTO</a>
+                        <label class="m-0">T:(+351)96 000 00 00</label>
+                        <label class="m-0">E: email@exemplo.com</label>
+                    </div>
+
+                    <!-- Pedro Oliveira -->
+                    <div class="col-md-4">
+                        <h6 class="mb-0">Pedro Oliveira</h6>
+                        <a class="lbln mt-0">Diretor de Marketing e comunicação da XPTO</a>
+                        <label class="m-0">T:(+351)96 000 00 00</label>
+                        <label class="m-0">E: email@exemplo.com</label>
+                    </div>
+
+                    <!-- Antonio Ribeiro -->
+                    <div class="col-md-4">
+                        <h6 class="mb-0">Antonio Ribeiro</h6>
+                        <a class="lbln mt-0">Diretor de Marketing e comunicação da XPTO</a>
+                        <label class="m-0">T:(+351)96 000 00 00</label>
+                        <label class="m-0">E: email@exemplo.com</label>
+                    </div>          
+                </div>
+            </div>
+            <div class="col-md-12">
+                <hr class="mb-2 mt-3">
+            </div>
+        </div>
+    </section>
+</body>
+<script src="./argon-design-system-master/assets/vendor/jquery/jquery.min.js"></script>
+<script src="./argon-design-system-master/assets/vendor/popper/popper.min.js"></script>
+<script src="./argon-design-system-master/assets/vendor/bootstrap/bootstrap.min.js"></script>
+<!-- Theme JS -->
+<script src="./argon-design-system-master/assets/js/argon.min.js"></script>
+<script src="./argon-design-system-master/assets/vendor/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script>
+
+</html>
